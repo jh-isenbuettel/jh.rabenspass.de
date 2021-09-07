8d160c9aea8663ed4bc91feebc79af4e3efa7342
thies <git@td00.de>
2021-09-07 20:31:40 +0200

    add postponed popup on page load

--- res/js/modal.js
+++ res/js/modal.js
@@ -17,6 +17,12 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
+// display it once the page is loaded
+window.onload = function() {
+    modal.style.display = "block";
+}
+
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
